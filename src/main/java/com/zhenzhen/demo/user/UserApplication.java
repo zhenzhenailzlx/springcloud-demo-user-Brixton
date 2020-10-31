18e7802ef492b8cab2332528741316940d7257ec
zhen.yin <zhen.yin@100credit.com>
2020-10-31 16:33:47 +0800

    修改bug

--- src/main/java/com/zhenzhen/demo/user/UserApplication.java
+++ src/main/java/com/zhenzhen/demo/user/UserApplication.java
@@ -2,21 +2,16 @@ package com.zhenzhen.demo.order;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
-
-import com.zhenzhen.demo.order.util.HttpClientUtil;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class OrderApplication {
+public class UserApplication {
 
 
 	public static void main(String[] args) {
 
-	    new SpringApplicationBuilder(OrderApplication.class).web(true).run(args);
+	    new SpringApplicationBuilder(UserApplication.class).web(true).run(args);
 	}
 	
 
